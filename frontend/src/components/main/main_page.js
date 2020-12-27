413561acd8be564a7b8bff4c5e2e91903152ac42
Dean Mayne <deancmayne@gmail.com>
2020-12-27 14:50:14 -0700

    first commit

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>A Twitter Clone</p>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
